378abd848ce01961da9179c671d8c1ba6c9af17a
Julia <jpearl@college.harvard.edu>
2020-03-25 13:50:50 -0400

    base case

--- matrix.c
+++ matrix.c
@@ -6,9 +6,12 @@
 #include <stdbool.h>
 
 void printMatrices(int **matrix, int dim);
-void matrixAddition(int **one, int **two, int dim, int positive);
-void matrixDestinationAddition(int **one, int r1, int c1, int **two, int r2, int c2, int**three, int r3, int c3, int dim, int positive);
+// void matrixAddition(int **one, int **two, int dim, int positive);
+void matrixDestinationAddition(int **one, int r1, int c1, int **two, int r2, int c2, int**three, int dim, int positive);
 int makeTwoMatrices(FILE *f, int dim, int** one, int** two);
+// int** matrixThirdAddition(int **one, int **two, int **third, int dim, int positive);
+int** matrixThirdsAddition(int a, int b, int c, int d, int pos_a, int pos_b, int pos_c, int pos_d, int ***p, int p_len, int **third, int r3, int c3, int dim);
+void matrixCorAddition(int **one, int **two, int r2, int c2, int dim, int positive);
 
 int makeTwoMatrices(FILE *f, int dim, int** one, int** two){
 	char ch, buffer[10];
@@ -66,19 +69,57 @@ void printMatrices(int **matrix, int dim){
 	}
 }
 
-void matrixAddition(int **one, int **two, int dim, int positive){
+
+void matrixDestinationAddition(int **one, int r1, int c1, int **two, int r2, int c2, int**three, int dim, int positive){
     for(int r = 0; r < dim; r++){
 		for (int c = 0; c < dim; c++){
-            one[r][c] += (two[r][c]*positive);
+            three[r][c] = one[r + r1][c + c1] + (two[r + r1][c + c1]*positive);
 		}
 	}
     
 }
-void matrixDestinationAddition(int **one, int r1, int c1, int **two, int r2, int c2, int**three, int r3, int c3, int dim, int positive){
+
+int** matrixThirdsAddition(int a, int b, int c, int d, int pos_a, int pos_b, int pos_c, int pos_d, int ***p, int p_len, int **third, int r3, int c3, int dim){
+    for (int i = 0; i < p_len; i++){
+        if (i == a){
+            matrixCorAddition(p[i], third, r3, c3, dim, pos_a);
+        } else if (i == b){
+            matrixCorAddition(p[i], third, r3, c3, dim, pos_b);
+        } else if (i == c){
+            matrixCorAddition(p[i], third, r3, c3, dim, pos_c);
+        } else if (i == d){
+            matrixCorAddition(p[i], third, r3, c3, dim, pos_d);
+        } 
+    }
+    return third;
+}
+
+void matrixCorAddition(int **one, int **two, int r2, int c2, int dim, int positive){
     for(int r = 0; r < dim; r++){
 		for (int c = 0; c < dim; c++){
-            three[r + r1][c + c1] = one[r + r1][c + c1] + (two[r + r1][c + c1]*positive);
+            two[r2 + r][c2 + c] += (one[r][c]*positive);
 		}
 	}
     
 }
+
+
+
+
+// void matrixAddition(int **one, int **two, int dim, int positive){
+//     for(int r = 0; r < dim; r++){
+// 		for (int c = 0; c < dim; c++){
+//             one[r][c] += (two[r][c]*positive);
+// 		}
+// 	}
+    
+// }
+
+// int** matrixThirdAddition(int **one, int **two, int **third, int dim, int positive){
+//     for(int r = 0; r < dim; r++){
+// 		for (int c = 0; c < dim; c++){
+//             third[r][c] = one[r][c] + (two[r][c]*positive);
+// 		}
+// 	}
+//     return third;
+// }
--- strassen.c
+++ strassen.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include "matrix.c"
 
+int **strassen(int **one, int **two, int **three, int r1, int c1, int r2, int c2, int r3, int c3, int dim);
 
 int main(int argc, char** argv) {
    // Ensure correct usage
@@ -33,8 +34,9 @@ int main(int argc, char** argv) {
    }
    makeTwoMatrices(f, dim, one, two);
    printf("\n \n");
-   matrixAddition(one, two, dim, 1);
-   printMatrices(one, dim);
+   
+   strassen(one, two, three, 0, 0, 0, 0, 0, 0, dim);
+   printMatrices(three, dim);
    for (int i = 0; i < dim; i ++){
       free(one[i]);
       free(two[i]);
@@ -57,7 +59,7 @@ tasks:
 2) based on n, either call strassens or call standard mult
 3) collect diagonals
 */
-int* big_mult(){
+int big_mult(){
 		return 0;
 }
 
@@ -69,6 +71,10 @@ tasks:
 void standard_mult(){
 
 }
+
+
+
+
 /*strassens algorithm
 RETURNS: ?
 tasks:
@@ -76,8 +82,113 @@ tasks:
 2) perform strassens -> recursive call to big_mult in it
 3) in strassen, add each term to the correct part of the third and final matrix
 */
-void strassen(){
+int **strassen(int **one, int **two, int **three, int r1, int c1, int r2, int c2, int r3, int c3, int dim){
+   int new_d = dim/2;
+   if (new_d == 1){
+      // actually multiply & add to three
+      int a = one[r1][c1], b = one[r1][c1 + 1], c = one[r1 + 1][c1], d = one[r1 + 1][c1 + 1];
+      int e = two[r2][c2], f = two[r2][c2 + 1], g = two[r2 + 1][c2], h = two[r2 + 1][c2 + 1];
+      int p1 = a*(f - h); 
+      int p2 = (a + b)*h;
+      int p3 = (c + d)*e;
+      int p4 = d*(g - e);
+      int p5 = (a + d)*(e + h);
+      int p6 = (b - d)*(g + h);
+      int p7 = (a - c)*(e + f);
+      three[r3][c3] = p5 + p4 - p2 + p6;
+      three[r3][c3 + 1]= p1 + p2;
+      three[r3 + 1][c3] = p3 + p4;
+      three[r3 + 1][c3 + 1] = p1 + p5 - p3 - p7;
+      return three;
+   } else {
+      int ***sums = (int***)malloc(10 * sizeof(int**));
+      for (int i = 0; i < 10; i++){
+         sums[i] = (int **)malloc(new_d * sizeof(int*));
+         for (int j = 0; j < new_d; j++){
+            sums[i][j] = (int *)malloc(new_d * sizeof(int));
+         }
+      }
+      for (int i = 0; i < 10; i++){
+         switch(i){
+            case 0: // f- h
+               matrixDestinationAddition(two, r2, c2 + new_d, two, r2 + new_d, c2 + new_d, sums[i], new_d, -1);
+               break;
 
+            case 1: // a + b
+               matrixDestinationAddition(one, r1, c1, one, r1, c1 + new_d, sums[i], new_d, 1);
+               break;
+            
+            case 2: //c + d
+               matrixDestinationAddition(one, r1 + new_d, c1, one, r1 + new_d, c1 + new_d, sums[i], new_d, 1);
+               break;
+            
+            case 3: //g - e
+               matrixDestinationAddition(two, r2 + new_d, c2, two, r2, c2, sums[i], new_d, -1);
+               break;
+
+            case 4: // a + d
+               matrixDestinationAddition(one, r1, c1, one, r1 + new_d, c1 + new_d, sums[i], new_d, 1);
+               break;
+
+            case 5: //e + h
+               matrixDestinationAddition(two, r2, c2, two, r2 + new_d, c2 + new_d, sums[i], new_d, 1);
+               break;
+
+            case 6: //b - d
+               matrixDestinationAddition(one, r1, c1 + new_d, one, r1 + new_d, c1 + new_d, sums[i], new_d, -1);
+               break;
+
+            case 7: // g + h
+               matrixDestinationAddition(two, r2 + new_d, c2, two, r2 + new_d, c2 + new_d, sums[i], new_d, -1);
+               break;
+
+            case 8: // a - c
+               matrixDestinationAddition(one, r1, c1, one, r1 + new_d, c1, sums[i], new_d, -1);
+               break;
+
+            default: //e + f
+               matrixDestinationAddition(two, r2, c2, two, r2, c2 + new_d, sums[i], new_d, 1);
+               break;
+         }  
+
+      }
+      int ***p = (int ***)malloc(7 * sizeof(int**));
+      for (int i = 0; i < 7; i++){
+         p[i] = (int**)malloc(new_d * sizeof(int*));
+         for (int j = 0; j < new_d; j++){
+            p[i][j] = (int *)malloc(new_d * sizeof(int));
+         }
+      }
+      p[0] = strassen(one, sums[0], p[0], r1, c1, 0, 0, 0, 0, new_d); 
+      p[1] = strassen(sums[1], two, p[1], 0, 0, r2 + new_d, c2 + new_d, 0, 0, new_d); 
+      p[2] = strassen(sums[2], two, p[2], 0, 0, r2, c2, 0, 0, new_d); 
+      p[3] = strassen(one, sums[3], p[3], r1 + new_d, c1 + new_d, 0, 0, 0, 0, new_d); 
+      p[4] = strassen(sums[4], sums[5], p[4], 0, 0, 0, 0, 0, 0, new_d); 
+      p[5] = strassen(sums[6], sums[7], p[5], 0, 0, 0, 0, 0, 0, new_d); 
+      p[6] = strassen(sums[8], sums[9], p[6], 0, 0, 0, 0, 0, 0, new_d); 
+      matrixThirdsAddition(5, 4, 2, 6, 1, 1, -1, 1, p, 7, three, r3, c3, new_d);
+      matrixThirdsAddition(1, 2, -1, -1, 1, 1, 1, 1, p, 7, three, r3, c3 + new_d, new_d);
+      matrixThirdsAddition(3, 4, -1, -1, 1, 1, 1, 1, p, 7, three, r3 + new_d, c3, new_d);
+      matrixThirdsAddition(1, 5, 3, 7, 1, 1, -1, -1, p, 7, three, r3 + new_d, c3 + new_d, new_d);
+      for (int i = 0; i < 10; i++){
+         for (int j = 0; j < new_d; j++){
+               free(sums[i][j]);
+         } 
+         free(sums[i]);
+      }
+      free(sums);
+      for (int i = 0; i < 7; i++){
+         for (int j = 0; j < new_d; j++){
+            free(p[i][j]);
+         }
+         free(p[i]);
+      }
+      free(p);
+      return three;
+
+   }
 }
+
+
 
 
